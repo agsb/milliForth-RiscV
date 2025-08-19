a4664b9658069aad25d0dc3a0d0547fbe0e7d647
agsb <barcellos.alvaro@gmail.com>
2025-08-19 13:33:21 -0300

    milli-forth states in svg

--- srcs/test0-riscv.f
+++ srcs/test0-riscv.f
@@ -34,18 +34,42 @@
  : nl 13 emit ;
  : spc 32 ;
 
- : dup sp @ @ ;
+ : dup sp @ @ @ ;
+
+ 1 2 4 8
+ 
+ sp
+
+ @
+
+ @
+
+ @
+
+ 16
+
+ dup 
+
+ dup 
+
+ 0
+
  : over sp @ cell + @ ;
+
+ over 
+
+ 0
+
 
  : invert dup nand ;
  : and nand invert ;
- : or invert swap invert and invert ;
  : - invert 1 + + ;
  : <> - 0# ;
  : = <> invert ;
  
  : drop dup - + ;
  : swap over over sp @ S3 + ! sp @ S2 + ! ;
+ : or invert swap invert and invert ;
 
  : 2dup over over ;
  : 2drop drop drop ;
@@ -55,8 +79,35 @@
 
  : 2* dup + ;
  : 2** 2* 2* 2* 2* 2* 2* 2* 2* ;
- : 80h 1 2* 2* 2* 2* 2* 2* 2* 2** 2** 2** ;
- : immediate latest @ cell + dup @ 80h or swap ! ;
+ : 80h 1 2* 2* 2* 2* 2* 2* 2* ;
+ : flag 80h 2** 2** 2** ;
+
+1
+2
+4
+8
+16
+32
++ + + + + 
+
+dup +
+dup +
+dup +
+dup +
+dup +
+dup +
+dup +
+dup +
+ 
+ 80h
+ 
+ 1
+
+ 2**
+
+ flag
+
+ : immediate last @ cell + dup @ flag or swap ! ;
  
  : >r rp @ @ swap rp @ ! rp @ cell - rp ! rp @ ! ;
  : r> rp @ @ rp @ cell + rp ! rp @ @ swap rp @ ! ;
@@ -64,11 +115,54 @@
  : branch rp @ @ dup @ + rp @ ! ;
  : ?branch 0# invert rp @ @ @ cell - and rp @ @ + cell + rp @ ! ;
  
- : lit rp @ @ dup CEL + rp @ ! @ ;
- : ['] rp @ @ dup CEL + rp @ ! @ ;
+ : lit rp @ @ dup cell + rp @ ! @ ;
+ : ['] rp @ @ dup cell + rp @ ! @ ;
  
  : rot >r swap r> swap ;
  
  : ] 1 s@ ! ;
  : [ 0 s@ ! ; 
 
+ immediate
+ 
+ : if ['] ?branch , here @ 0 , ; immediate
+ : then dup here @ swap - swap ! ; immediate
+ : else ['] branch , here @ 0 , swap dup 
+     here @ swap - swap ! ; immediate
+ 
+ : begin here @ ; immediate
+ : again ['] branch , here @ - , ; immediate
+ : until ['] ?branch , here @ - , ; immediate
+ : while ['] ?branch , here @ 0 , ; immediate
+ : repeat swap ['] branch , here @ - , 
+     dup here @ swap - swap ! ; immediate
+ 
+ : do here @ ['] >r , ['] >r , ; immediate
+ : loop ['] r> , ['] r> , ['] lit , 1 , ['] + , 
+     ['] 2dup , ['] = , ['] ?branch , 
+     here @ - , ['] 2drop , ; immediate
+ 
+ : bl lit [ 1 2* 2* 2* 2* 2* , ] ;
+ : 0fh lit [ 4 4 4 4 + + + 1 - , ] ;
+ : ffh lit [ 0fh 2* 2* 2* 2* 0fh or , ] ;
+ : c@ @ ffh and ;
+ : type 0 do dup c@ emit 1 + loop drop ;
+ 
+ : in> >in @ c@ >in dup @ 1 + swap ! ;
+ : parse in> drop >in @ swap 0 begin over in> 
+     <> while 1 + repeat swap bl 
+     = if >in dup @ 1 - swap ! then ;
+ : word in> drop begin dup in> <> until >in @ 2 - >in ! parse ;
+ 
+ : [char] ['] lit , bl word drop c@ , ; immediate
+ : ." [char] " parse type ; immediate
+ 
+ : ( [char] ) parse drop drop ; immediate
+ 
+ ." Hello world " cr
+ 
+ ." That's all Folks !" cr
+ 
+ dump
+ 
+ 
