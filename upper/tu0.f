8500fdca9bb8ca587bda25daf6419542ba2f8d95
agsb <barcellos.alvaro@gmail.com>
2025-10-31 12:07:16 -0300

    trying for s" as is

--- upper/tu0.f
+++ upper/tu0.f
@@ -178,3 +178,19 @@
  
  ." THAT'S ALL FOLKS !" CR
 
+ : ," 0 DO DUP C@ , 1 + LOOP DROP ;
+
+ : COMPILES 
+        ['] LIT ,
+        HERE >R
+        0 ,
+        ['] LIT ,
+        DUP ,
+        ['] TYPE ,
+        ['] EXIT ,
+        HERE R> !
+        ,S 0 ,
+        ;
+
+ : S" [CHAR] " PARSE COMPILES ; IMMEDIATE
+
