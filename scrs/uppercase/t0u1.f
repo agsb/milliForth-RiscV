9d0308a61741f438be28045486899da48d8e5d2d
agsb <barcellos.alvaro@gmail.com>
2026-03-10 13:02:26 -0300

    review of code, small changes, better comments and named constants

--- scrs/uppercase/t0u1.f
+++
@@ -1,9 +0,0 @@
-
- : TYPE, 0 DO DUP C@ , 1 + LOOP DROP  ; SEE 
-
- : ," STATE 
-        IF [CHAR] " PARSE TYPE, 
-        ELSE  
-        R> BEGIN DUP C@ DUP [CHAR] " = UNTIL >R 
-        THEN ; SEE 
-
